c99c15746e16cf81cf0175bb7cfd9536e5abc6ea
johannesschmolke <johannes.schmolke2006@gmail.com>
2025-08-18 21:09:17 +0200

    version 1

---
+++ Script.js
@@ -0,0 +1,52 @@
+window.onscroll = function() {shrinkHeader()};
+
+function shrinkHeader() {
+  const header = document.getElementById("header");
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    header.classList.add("small");
+  } else {
+    header.classList.remove("small");
+  }
+}
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+setInterval(function() {
+  slideIndex++;
+  if (slideIndex > document.getElementsByClassName("mySlides").length) {
+    slideIndex = 1;
+  }
+  showSlides(slideIndex);
+}, 15000);
+
